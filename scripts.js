3d1cd7c77c0c1eb72287623706a72e2469037d5b
SevenCubed <78549436+SevenCubed@users.noreply.github.com>
2021-03-19 15:56:17 +0100

    Progress!

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-/*TODO      - manage proper gamestate to allow for multiple successive games.
-            - give the player proper UI feedback on the game.
-            https://totalnonsense.com/download/download-vector-playing-cards/
+/*Notes     - give the player proper UI feedback on the game.
+            The front card graphics were taken from https://totalnonsense.com/download/download-vector-playing-cards/
 */
 //Globals & Classes
 const ranks = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ');
@@ -11,8 +10,13 @@ const hitButton = document.getElementById('btnHit');
 const stayButton = document.getElementById('btnStay');
 const dealButton = document.getElementById('btnDeal');
 const wipeButton = document.getElementById('btnWipe');
+const newGameButton = document.getElementById('btnNewGame');
 const cardBack = "back_script_1"
 let game, deck, human, dealer; //the variables needed for the newGame()
+newGameButton.addEventListener('click', newGame);
+dealButton.addEventListener('click', firstDeal);
+hitButton.addEventListener('click', hit);
+stayButton.addEventListener('click', stay);
 
 class Card {
     constructor(name, suit, rank, value, color, bitValue) {
@@ -40,13 +44,20 @@ class Card {
         deckDiv[0].appendChild(this.div);
     }
     flip() {
-        if (this.closed == true) {
-            this.div.setAttribute("style", `background-image: url("img/${this.rank + this.suit}.svg"`);
-        }
-        else {
-            this.div.setAttribute("style", `background-image: url("img/${cardBack}.svg"`);
-        }
-        this.closed = !this.closed
+        this.div.classList.add("flip")
+        setTimeout(() => { this.div.classList.remove("flip") }, 600);
+        setTimeout(() => {
+            if (this.closed == true) {
+                this.div.setAttribute("style", `background-image: url("img/${this.rank + this.suit}.svg"`);
+            }
+            else {
+                this.div.setAttribute("style", `background-image: url("img/${cardBack}.svg"`);
+            }
+            this.closed = !this.closed
+        }, 200)
+        //this kind of looks like a mess, but it works.. The effect is purely visual, the actual value of the card is stored inside .value,
+        //and that's not altered. So even if the animation breaks or messes up, the actual functionality of the game shouldn't alter.
+        //By timing the swapping of the background image to the exact moment the card div is 90 degrees transformed, it creates the illusion of an actual card flip.
     }
 }
 
@@ -63,8 +74,8 @@ class Deck {
             v = r + 1 < 10 ? r + 1 : 10; //value, capping at 10 for the face cards
             v = v == 1 ? 11 : v; //setting the Ace at a value of 11, as it is only necessary for it to be a 1 should the totals be over 21
             s = suits[Math.floor(i / 13)] //suit
-            c = [i / 13 | 0] % 2 ? 'red' : 'black'; //color
-            n = `${fullRanks[r]} of ${fullSuits[i / 13 | 0]}` //name
+            c = [i / 13 | 0] % 2 ? 'red' : 'black'; //color, unimportant if I use images as cards, but nice to keep track of.
+            n = `${fullRanks[r]} of ${fullSuits[i / 13 | 0]}` //Full name, a leftover from early stages
             b = Math.pow(2, r); //bit value of the rank
             this.deck.push(new Card(n, s, ranks[r], v, c, b));
         }
@@ -85,7 +96,7 @@ class Deck {
         if (this.deck.length == 0) { this.create(); this.shuffle(); } /*Should the deck ever run out in the midst of a draw, it'll make a new deck and then shuffle it
         A side effect of this is that the "create" function does not discriminate based on cards already in the field, 
         so in rare situations it may end up creating then popping a card they already have on the field.
-        Realistically this should never happen if I make sure the deck is refreshed every 5-6 games, as in a standard casino.*/
+        Realistically this should never happen if I make sure the deck is refreshed every 2-5 games, as in a standard casino.*/
         return this.deck.pop();
     }
 }
@@ -119,16 +130,19 @@ class Game {
 }
 
 //New game
-
-function newGame(){
-game = new Game();
-deck = new Deck();
-human = new Player('human');
-dealer = new Player('dealer');
-dealButton.addEventListener('click', firstDeal);
-document.getElementById('btnSimulate').addEventListener('click', simulate)
-wipeButton.addEventListener('click', wipe);
-return game, deck, human, dealer;
+function newGame() {
+    game = new Game();
+    deck = new Deck();
+    human = new Player('Human');
+    dealer = new Player('Dealer');
+    wipe();
+    dealButton.disabled = false;
+    hitButton.disabled = true;
+    stayButton.disabled = true
+    const winDivs = document.getElementsByClassName("nameheader");
+    winDivs[0].innerHTML = `${dealer.name} (${dealer.wins})`
+    winDivs[1].innerHTML = `${human.name} (${human.wins})`
+    return game, deck, human, dealer;
 }
 function wipe() {
     let div = document.getElementsByClassName('deck')[0];
@@ -141,26 +155,19 @@ function wipe() {
     }
     human.reset();
     dealer.reset();
-    dealButton.addEventListener('click', firstDeal);
 }
 
 function firstDeal() {
-    dealButton.removeEventListener('click', firstDeal);
-   // human.hand.push(deck.deal());
-   // dealer.hand.push(deck.deal());
-   // human.hand.push(deck.deal());
-   // dealer.hand.push(deck.deal());
+    wipe()
+    dealButton.disabled = true; //turn off the deal button to transition to the hit/stay phase
+    hitButton.disabled = false; //this is the same as removeAttribute? but it seems to play nicer when trying to ADD the disabled
+    stayButton.disabled = false;
     for (i = 0; i < 2; i++) {
         human.hand.push(deck.deal());
         dealer.hand.push(deck.deal());
         human.hand[i].draw('deck');
         i == 1 ? dealer.hand[i].draw('dealer', true) : dealer.hand[i].draw('dealer')
     }
-    //for (i = 0; i < dealer.hand.length; i++) { i == 1 ? dealer.hand[i].draw('dealer', true) : dealer.hand[i].draw('dealer') }
-
-  //  dealer.hand[0].draw('dealer')
-   // dealer.hand[1].draw('dealer', true)
-
     if ((human.hand[0].value == 11 && human.hand[1].value == 10) || (human.hand[0].value == 10 && human.hand[1].value == 11)) {
         //Should the player ever get Blackjack on the draw, that's an instant victory.. Except if they both have one.
         human.blackjack = true;
@@ -169,9 +176,11 @@ function firstDeal() {
         //Should the dealer ever get Blackjack on the draw but the player hasn't, that's an instant defeat.
         dealer.blackjack = true;
     }
-    if (dealer.blackjack || human.blackjack) { eval() }; //go straight to score evaluation.
-    hitButton.addEventListener('click', hit);
-    stayButton.addEventListener('click', stay);
+    if (dealer.blackjack || human.blackjack) {
+        stayButton.disabled = true;
+        hitButton.disabled = true;
+        eval()  //go straight to score evaluation.
+    };
     console.log(human.total)
     console.log(dealer.total)
 }
@@ -186,7 +195,8 @@ function hit() {
             human.total -= 10
         }
         else {
-            bust();
+            hitButton.disabled = true;
+            stayButton.disabled = true;
             human.bust = true;
             eval();
         }
@@ -194,7 +204,8 @@ function hit() {
 }
 
 function stay() {
-    dealer.hand[1].flip();
+    hitButton.disabled = true;
+    stayButton.disabled = true;
     //dealer.hand[1].draw('dealer');
     while ((dealer.total < 17 || (dealer.total < 16 && human.total == 16)) && !human.bust && !human.blackjack) {
         dealer.hand.push(deck.deal());
@@ -213,6 +224,7 @@ function stay() {
     eval();
 }
 function eval() {
+    dealer.hand[1].flip(); //the dealer must show the card even on a bust, so I'm putting this here even if it's not exactly the order of proper blackjack operations.
     switch (true) {
         case (human.blackjack && dealer.blackjack):
             console.log('Draw: Both the player as the dealer have a blackjack! What are the odds!?');
@@ -234,10 +246,10 @@ function eval() {
             console.log('Victory: Dealer busted.');
             human.wins++
             break;
-    /*  case (human.hand.length >=5 && !humanbust):
-            console.log('Victory: Five card trick! ... But this is a casino game, so we don't use that rule.)
-            human.wins++
-            break;      */
+        /*  case (human.hand.length >=5 && !humanbust):
+                console.log('Victory: Five card trick! ... But this is a casino game, so we don't use that rule.)
+                human.wins++
+                break;      */
         case (dealer.total == human.total):
             console.log('Draw: Dealer scored equal to the player.');
             game.draws++
@@ -253,16 +265,15 @@ function eval() {
     }
     game.round++
     game.shuffle++
-    if(Math.random()<((game.shuffle-1)/4)){ //Between every 2 to 5 games, with increasing likeliness from 25%-100%, the deck will be shuffled.
+    if (Math.random() < ((game.shuffle - 1) / 4)) { //Between every 2 to 5 games, with increasing likeliness from 25%-100%, the deck will be shuffled.
         deck.shuffle();
         game.shuffle = 0
         console.log('The deck has been shuffled.')
     }
-    hitButton.removeEventListener('click', hit);
-    stayButton.removeEventListener('click', stay);
-}
-function bust() {
-
+    const winDivs = document.getElementsByClassName("nameheader");
+    winDivs[0].innerHTML = `${dealer.name} (${dealer.wins})`
+    winDivs[1].innerHTML = `${human.name} (${human.wins})`
+    dealButton.disabled = false;
 }
 
 function simulate() {
