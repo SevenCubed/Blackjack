4864bafb4072efd18af38fd304e0144b983939b7
SevenCubed <78549436+SevenCubed@users.noreply.github.com>
2021-03-19 14:37:00 +0100

    .

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-/*TODO      - One of the dealer cards is hidden
-            - manage proper gamestate to allow for multiple successive games.
+/*TODO      - manage proper gamestate to allow for multiple successive games.
+            - give the player proper UI feedback on the game.
+            https://totalnonsense.com/download/download-vector-playing-cards/
 */
+//Globals & Classes
 const ranks = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ');
 const fullRanks = 'Ace Two Three Four Five Six Seven Eight Nine Ten Jack Queen King'.split(' ');
 const suits = '♠︎ ♥︎ ♣︎ ♦︎'.split(' ');
@@ -9,6 +11,8 @@ const hitButton = document.getElementById('btnHit');
 const stayButton = document.getElementById('btnStay');
 const dealButton = document.getElementById('btnDeal');
 const wipeButton = document.getElementById('btnWipe');
+const cardBack = "back_script_1"
+let game, deck, human, dealer; //the variables needed for the newGame()
 
 class Card {
     constructor(name, suit, rank, value, color, bitValue) {
@@ -18,24 +22,31 @@ class Card {
         this.value = value; //for Blackjack, the 10 K Q and J cards are all a value of 10, while the Ace can be 1 or 11. 
         this.color = color;
         this.bitValue = bitValue; //If I use bit values, I can recognize the exact individual cards regardless of the result and future proof for future projects like poker or something.
+        this.div = null; //Apparently you use null to represent a value that's intentionally empty, as opposed to undefined?
+        this.closed = null;
     }
     draw(location, closed) {
-        const newDiv = document.createElement("div");
+        this.div = document.createElement("div");
         const deckDiv = document.getElementsByClassName(location);
-        newDiv.setAttribute('class', 'card')
+        this.div.setAttribute('class', 'card')
+
         if (closed != true) {
-            newDiv.appendChild(document.createTextNode(this.rank));
-            newDiv.appendChild(document.createTextNode(this.suit));
-            newDiv.setAttribute("style", `color: ${this.color}`);
+            this.div.setAttribute("style", `background-image: url("img/${this.rank + this.suit}.svg"`);
         }
         else {
-            newDiv.setAttribute('class', 'back card');
-            newDiv.appendChild(document.createTextNode("."));
-            newDiv.appendChild(document.createTextNode(""));
-            newDiv.setAttribute("style", `color: ${this.color}`);
-            //newDiv.setAttribute('class', 'back card')
+            this.div.setAttribute("style", `background-image: url("img/${cardBack}.svg"`);
         }
-        deckDiv[0].appendChild(newDiv);
+        this.closed = closed;
+        deckDiv[0].appendChild(this.div);
+    }
+    flip() {
+        if (this.closed == true) {
+            this.div.setAttribute("style", `background-image: url("img/${this.rank + this.suit}.svg"`);
+        }
+        else {
+            this.div.setAttribute("style", `background-image: url("img/${cardBack}.svg"`);
+        }
+        this.closed = !this.closed
     }
 }
 
@@ -101,17 +112,24 @@ class Player {
 
 class Game {
     constructor() {
+        this.round = 0
+        this.shuffle = 0
+        this.draws = 0
     }
 }
 
-let deck = new Deck();
-let human = new Player('human');
-let dealer = new Player('dealer');
+//New game
 
+function newGame(){
+game = new Game();
+deck = new Deck();
+human = new Player('human');
+dealer = new Player('dealer');
 dealButton.addEventListener('click', firstDeal);
 document.getElementById('btnSimulate').addEventListener('click', simulate)
 wipeButton.addEventListener('click', wipe);
-
+return game, deck, human, dealer;
+}
 function wipe() {
     let div = document.getElementsByClassName('deck')[0];
     while (div.firstChild) {
@@ -128,17 +146,20 @@ function wipe() {
 
 function firstDeal() {
     dealButton.removeEventListener('click', firstDeal);
-    human.hand.push(deck.deal());
-    dealer.hand.push(deck.deal());
-    human.hand.push(deck.deal());
-    dealer.hand.push(deck.deal());
-    for (i = 0; i < human.hand.length; i++) {
+   // human.hand.push(deck.deal());
+   // dealer.hand.push(deck.deal());
+   // human.hand.push(deck.deal());
+   // dealer.hand.push(deck.deal());
+    for (i = 0; i < 2; i++) {
+        human.hand.push(deck.deal());
+        dealer.hand.push(deck.deal());
         human.hand[i].draw('deck');
+        i == 1 ? dealer.hand[i].draw('dealer', true) : dealer.hand[i].draw('dealer')
     }
     //for (i = 0; i < dealer.hand.length; i++) { i == 1 ? dealer.hand[i].draw('dealer', true) : dealer.hand[i].draw('dealer') }
 
-    dealer.hand[0].draw('dealer')
-    dealer.hand[1].draw('dealer', true)
+  //  dealer.hand[0].draw('dealer')
+   // dealer.hand[1].draw('dealer', true)
 
     if ((human.hand[0].value == 11 && human.hand[1].value == 10) || (human.hand[0].value == 10 && human.hand[1].value == 11)) {
         //Should the player ever get Blackjack on the draw, that's an instant victory.. Except if they both have one.
@@ -173,9 +194,8 @@ function hit() {
 }
 
 function stay() {
-    const secondCard = document.getElementsByClassName('dealer')[0]
-    secondCard.removeChild(secondCard.getElementsByClassName('card')[1]) //"flip" the second card 
-    dealer.hand[1].draw('dealer')
+    dealer.hand[1].flip();
+    //dealer.hand[1].draw('dealer');
     while ((dealer.total < 17 || (dealer.total < 16 && human.total == 16)) && !human.bust && !human.blackjack) {
         dealer.hand.push(deck.deal());
         let valueMap = dealer.hand.map(card => card.value)
@@ -188,7 +208,7 @@ function stay() {
             else {
                 dealer.bust = true;
             }
-        }
+        }//It  does not seem to be possible in JS to pause the script for more fluid "drawing" of the dealer's cards without completely hogging the CPU with some kind of while/newDate loop
     }
     eval();
 }
@@ -196,6 +216,7 @@ function eval() {
     switch (true) {
         case (human.blackjack && dealer.blackjack):
             console.log('Draw: Both the player as the dealer have a blackjack! What are the odds!?');
+            game.draws++
             break;
         case (human.blackjack):
             console.log('Victory: Player won through blackjack.');
@@ -213,9 +234,13 @@ function eval() {
             console.log('Victory: Dealer busted.');
             human.wins++
             break;
+    /*  case (human.hand.length >=5 && !humanbust):
+            console.log('Victory: Five card trick! ... But this is a casino game, so we don't use that rule.)
+            human.wins++
+            break;      */
         case (dealer.total == human.total):
             console.log('Draw: Dealer scored equal to the player.');
-            //Apparently ties in Blackjack are a unique case that vary per table? 
+            game.draws++
             break;
         case (dealer.total > human.total):
             console.log('Defeat: Dealer scored higher than the player.');
@@ -226,8 +251,13 @@ function eval() {
             human.wins++
             break;
     }
-    console.log(human.wins)
-    console.log(dealer.wins)
+    game.round++
+    game.shuffle++
+    if(Math.random()<((game.shuffle-1)/4)){ //Between every 2 to 5 games, with increasing likeliness from 25%-100%, the deck will be shuffled.
+        deck.shuffle();
+        game.shuffle = 0
+        console.log('The deck has been shuffled.')
+    }
     hitButton.removeEventListener('click', hit);
     stayButton.removeEventListener('click', stay);
 }
@@ -235,7 +265,6 @@ function bust() {
 
 }
 
-//firstDeal()
 function simulate() {
     console.time('timing');
     let draw = 0, defeat = 0, victory = 0, sims = 10000;
@@ -258,3 +287,4 @@ function simulate() {
     console.log(`There's a ${((defeat + victory + draw) / sims * 100).toFixed(2)}% chance of blackjack and a ${(draw / sims * 100).toFixed(2)}% chance for a draw.`)
     console.timeEnd('timing');
 }
+newGame();
